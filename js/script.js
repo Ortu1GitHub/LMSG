a498072387ecd2eef46ae2d02a15b78408ab0542
Ortu1GitHub <jooror@hotmail.com>
2024-05-15 14:08:08 +0200

    Añadido slider con 2 imagenes

--- js/script.js
+++ js/script.js
@@ -16,3 +16,33 @@ function cambiarModoOscuro() {
         alert("Se ha cambiado a Modo Oscuro");
     }
 }
+
+//Funcion cambiaImagenes para generar el slider
+window.addEventListener('load', function(){
+
+    let imagenes = [];
+    imagenes[0] = 'media/img/peter2.jpg';
+    imagenes[1] = 'media/img/peterNew.jpg';
+
+    let index = 0;
+
+    function cambiarImagenes(){
+        document.getElementById('slider').src = imagenes[index];
+
+        if(index === 0){
+            index++;
+        } else {
+            index--;
+        }
+    }
+
+    // Cambiar la imagen inicial por la primera imagen del slider
+    document.getElementById('slider').src = imagenes[0];
+
+    // Inicia el ciclo después de un breve retraso para dar tiempo a la carga de la imagen inicial
+    setTimeout(function() {
+        setInterval(cambiarImagenes, 500);
+        //Espera de 1 s para iniciar el slider
+    }, 1000); 
+
+});
